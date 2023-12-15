a4cdf039b002a8d46a059894eee9ae5acd9fb394
ahmedhaddan <ahmed.haddan.info@gmail.com>
2023-12-15 03:44:21 +0100

    init

--- src/FileCabinet/SuiteScripts/restHelloworld.js
+++ src/FileCabinet/SuiteScripts/restHelloworld.js
@@ -2,32 +2,18 @@
  * @NApiVersion 2.x
  * @NScriptType Restlet
  */
-define(['N/https', 'N/log'], function (https, log) {
+define(['N/log'], function (log) {
 
     function doPost(requestBody) {
-        try {
-            var githubToken = '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';
-            var owner = 'ahmedhaddan';
-            var repo = 'Netsuite_Training';
+        log.debug('Received POST Request', requestBody);
 
-            var response = https.post({
-                url: 'https://api.github.com/repos/' + owner + '/' + repo + '/dispatches',
-                body: JSON.stringify({
-                    event_type: 'trigger-hello-world', // This should match your GitHub Actions configuration
-                    client_payload: { message: 'Hello World' }
-                }),
-                headers: {
-                    'Authorization': 'token ' + githubToken,
-                    'Content-Type': 'application/json',
-                    'User-Agent': 'ahmedhaddan' // GitHub API requires a User-Agent header
-                }
-            });
+        // You can process requestBody here as needed
 
-            return response.body;
-        } catch (error) {
-            log.error('Error', error);
-            return error.toString();
-        }
+        return {
+            status: 'Success',
+            message: 'POST request processed successfully',
+            receivedData: requestBody
+        };
     }
 
     return {
